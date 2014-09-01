62a9c152bce4020ec4e5053c03a3da887add95b3
Wormzer <Wormzer@wormzer_envy.hsd1.pa.comcast.net>
2014-09-01 07:23:54 -0400

    1.0.0.7 - Added missing GMS resources and App.cs

---
+++ GameMaker/Resource/GMShader.cs
@@ -0,0 +1,146 @@
+﻿#region MIT
+
+// 
+// GMare.
+// Copyright (C) 2011, 2012, 2013, 2014 Michael Mercado
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+
+#endregion
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Collections.Generic;
+using GameMaker.Common;
+
+namespace GameMaker.Resource
+{
+    [Serializable]
+    public class GMShader : GMResource
+    {
+        #region Fields
+
+        public const string Divider = @"//######################_==_YOYO_SHADER_MARKER_==_######################@~//";
+        private string _code = "";
+        private string _type = "";
+
+        #endregion
+
+        #region Properties
+
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public GMShader()
+        {
+            // Set base code
+            _code = @"//
+                    // Simple passthrough vertex shader
+                    //
+                    attribute vec3 in_Position;                  // (x,y,z)
+                    //attribute vec3 in_Normal;                  // (x,y,z)     unused in this shader.	
+                    attribute vec4 in_Colour;                    // (r,g,b,a)
+                    attribute vec2 in_TextureCoord;              // (u,v)
+
+                    varying vec2 v_vTexcoord;
+                    varying vec4 v_vColour;
+
+                    void main()
+                    {
+                    vec4 object_space_pos = vec4( in_Position.x, in_Position.y, in_Position.z, 1.0);
+                    gl_Position = gm_Matrices[MATRIX_WORLD_VIEW_PROJECTION] * object_space_pos;
+    
+                    v_vColour = in_Colour;
+                    v_vTexcoord = in_TextureCoord;
+                    }
+
+                    //######################_==_YOYO_SHADER_MARKER_==_######################@~//
+                    // Simple passthrough fragment shader
+                    //
+                    varying vec2 v_vTexcoord;
+                    varying vec4 v_vColour;
+
+                    void main()
+                    {
+                    gl_FragColor = v_vColour * texture2D( gm_BaseTexture, v_vTexcoord );
+                    }";
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Reads shaders from Game Maker Studio project file
+        /// </summary>
+        /// <param name="directory">The directory the shaders are placed</param>
+        /// <param name="assets">Collection of project assets</param>
+        public static GMList<GMShader> ReadShadersGMX(string directory, ref List<string> assets)
+        {
+            // A list of shader
+            GMList<GMShader> shaders = new GMList<GMShader>();
+            shaders.AutoIncrementIds = false;
+
+            // Iterate through .gmx files in the directory
+            foreach (string file in Directory.GetFiles(directory, "*.shader"))
+            {
+                // Set name of the shader
+                string name = GetResourceName(file);
+
+                // If the file is not in the asset list, it has been orphaned, continue
+                if (!assets.Contains(name))
+                    continue;
+
+                // Create a stream to the shader file
+                using (StreamReader streamReader = new StreamReader(file))
+                {
+                    // Create a new shader
+                    GMShader shader = new GMShader();
+                    shader.Name = name;
+                    shader.Id = GetIdFromName(name);
+                    shader.Code = streamReader.ReadToEnd();
+                    shader.Type = name;
+                    
+                    // Add the shader
+                    shaders.Add(shader);
+                }
+            }
+
+            // Return the list of shaders
+            return shaders;
+        }
+
+        #endregion
+    }
+}
---
+++ Objects/App.cs
@@ -0,0 +1,480 @@
+#region MIT
+
+// 
+// GMare.
+// Copyright (C) 2011, 2012, 2013, 2014 Michael Mercado
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+
+#endregion
+
+using System;
+using System.IO;
+using System.Drawing;
+using System.Reflection;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using GameMaker.Common;
+using GameMaker.Project;
+using GameMaker.Resource;
+using GMare.Forms;
+
+namespace GMare.Objects
+{
+    public class App
+    {
+        #region Fields
+
+        public static int TILE_EMPTY = -1;                                             // The number for an empty tile id
+        public static GMareRoom Room = null;                                           // Global edit room
+        public static bool Changed = false;                                            // If the project changed
+        public static string SavePath = string.Empty;                                  // A valid save path for the current project
+        public static string UndoRedoMaximumAppKey = "UndoRedoMaximum";                // Undo/Redo maximum app.config settings key
+        public static string LowerLayerBrightnessAppKey = "LowerLayerBrightness";      // Lower layer brightness app.config settings key
+        public static string UpperLayerTransparencyAppKey = "UpperLayerTransparency";  // Upper layer transparency app.config settings key
+        public static string ShowScaleWarningAppKey = "ShowScaleWarning";              // Scale warning app.config settings key
+        public static string ShowBlendWarningAppKey = "ShowBlendWarning";              // Blend color warning app.config settings key
+        public static string ShowTipsAppKey = "ShowTips";                              // If showing tips app.config settings key
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets the applications global configuration settings
+        /// </summary>
+        /// <returns>A configuration file</returns>
+        public static Configuration GetConfig(bool supressWarning)
+        {
+            // Local variables acquiring config file
+            string appPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string configPath = Path.Combine(appPath, "app.config");
+
+            // If the app.config file does not exist, and not supressing the warning, notify the user and return
+            if (!File.Exists(configPath) && !supressWarning)
+            {
+                MessageBox.Show("GMare could not find the file: app.config. Please make sure it resides in the same directory as GMare.",
+                    "GMare", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return null;
+            }
+
+            // Get the config file and return it
+            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+            fileMap.ExeConfigFilename = configPath;
+            return ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+        }
+
+        /// <summary>
+        /// Gets the undo/redo maximum from app.config
+        /// </summary>
+        /// <returns></returns>
+        public static int GetUndoRedoMax(bool supressWarning)
+        {
+            // Get the config file
+            Configuration config = App.GetConfig(supressWarning);
+
+            // If the config file was not found, return default
+            if (config == null || config.AppSettings.Settings.Count == 0)
+                return 10;
+
+            try
+            {
+                // Get the undo/redo max setting
+                int undoRedoMaximum;
+                bool result = int.TryParse(config.AppSettings.Settings[UndoRedoMaximumAppKey].Value, out undoRedoMaximum);
+                return result ? undoRedoMaximum + 1 : 10;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error retrieving app setting: " + UndoRedoMaximumAppKey + ", app.config may be corrupted.", "GMare", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return 10;
+            }
+        }
+
+        /// <summary>
+        /// Gets a flag value from the app.config
+        /// </summary>
+        /// <returns>The app.config flag value</returns>
+        public static bool GetConfigFlag(string key, bool defaultValue)
+        {
+            // Get the config file
+            Configuration config = App.GetConfig(true);
+
+            // If the config file was not found, return default
+            if (config == null || config.AppSettings.Settings.Count == 0)
+                return defaultValue;
+
+            // Get flag
+            bool flag;
+            bool success = bool.TryParse(config.AppSettings.Settings[key].Value, out flag);
+            return success ? flag : defaultValue;
+        }
+
+        /// <summary>
+        /// Gets a flag value from the app.config
+        /// </summary>
+        /// <returns>The app.config flag value</returns>
+        public static void SetConfigFlag(string key, bool val)
+        {
+            // Get the config file
+            Configuration config = App.GetConfig(true);
+
+            // If the config file was not found, return default
+            if (config == null || config.AppSettings.Settings.Count == 0)
+                return;
+
+            try
+            {
+                // Set the config with user values
+                config.AppSettings.Settings[key].Value = val.ToString();
+                config.Save();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error saving the app setting, app.config may be corrupted.", "GMare", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        /// <summary>
+        /// Saves a project file
+        /// </summary>
+        /// <param name="file">Path to save to</param>
+        public static bool SaveProject(string file)
+        {
+            // If a project was loaded
+            if (Room == null)
+            {
+                MessageBox.Show("There was no loaded project to save.");
+                return false;
+            }
+
+            // If the file does not exist
+            if (file == string.Empty)
+            {
+                // Create a save file dialog
+                using (SaveFileDialog form = new SaveFileDialog())
+                {
+                    // Set filter
+                    form.Filter = "GMare Project File (.gmpx)|*.gmpx";
+                    form.Title = "Save Project As...";
+
+                    // If the dialog result was Ok, save the project
+                    if (form.ShowDialog() != DialogResult.OK)
+                        return false;
+
+                    // Save the project path
+                    App.SavePath = form.FileName;
+                    file = form.FileName;
+                }
+            }
+
+            // Create a new stream writer, write project xml
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(file))
+            {
+                System.Xml.Serialization.XmlSerializer writer = new System.Xml.Serialization.XmlSerializer(typeof(GMareRoom));
+                writer.Serialize(sw, Room);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Loads a project from a support file type
+        /// </summary>
+        /// <param name="file">The path pointing to a supported file</param>
+        public static void SetProject(string file)
+        {
+            // Get file extension
+            string ext = file.Substring(file.LastIndexOf('.')).ToLower();
+
+            // Image file
+            if (ext == ".png" || ext == ".bmp" || ext == ".gif")
+            {
+                // Create a new file stream
+                using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+                {
+                    // Create a new image import form
+                    using (ImportImageForm form = new ImportImageForm((Bitmap)Bitmap.FromStream(fs)))
+                    {
+                        // If Ok was clicked from the dialog, set room
+                        if (form.ShowDialog() == DialogResult.OK)
+                            Room = form.NewRoom;
+
+                        return;
+                    }
+                }
+            }
+
+            // Create a new stream reader, read in standard xml project file
+            using (StreamReader sr = new StreamReader(file))
+            {
+                XmlSerializer writer = new XmlSerializer(typeof(GMareRoom));
+                Room = (GMareRoom)writer.Deserialize(sr);
+            }
+
+            // If the room was not loaded successfully, inform the user
+            if (Room == null)
+            {
+                MessageBox.Show("There was a problem loading the project data. The file may be invalid or corrupt.", "GMare", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+            // Remove default background
+            Room.Backgrounds.RemoveAt(0);
+
+            // Set edit room and save path, update the UI
+            App.SavePath = file;
+        }
+
+        /// <summary>
+        /// Gets a GMare room from a project file
+        /// </summary>
+        /// <param name="file">The path to the GMare project file</param>
+        /// <returns>A GMare room</returns>
+        public static GMareRoom GetProject(string file)
+        {
+            // Get file extension
+            string ext = file.Substring(file.LastIndexOf('.')).ToLower();
+
+            // If not a gmpx file
+            if (ext != ".gmpx")
+                return null;
+
+            // Create a new room
+            GMareRoom room = null;
+
+            // Create a new stream reader, read in standard xml project file
+            using (StreamReader sr = new StreamReader(file))
+            {
+                XmlSerializer writer = new XmlSerializer(typeof(GMareRoom));
+                room = (GMareRoom)writer.Deserialize(sr);
+            }
+
+            // If the room was not loaded successfully, inform the user
+            if (room == null)
+            {
+                MessageBox.Show("There was a problem loading the project data. The file may be invalid or corrupt.", "GMare", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return null;
+            }
+
+            // Remove default background
+            room.Backgrounds.RemoveAt(0);
+
+            // Return the opened room
+            return room;
+        }
+
+        /// <summary>
+        /// Gets data from a Game Maker project file
+        /// </summary>
+        /// <param name="file">The path to the Game Maker project file</param>
+        /// <returns>A GM project</returns>
+        public static GMProject GetGMProject(string file)
+        {
+            // Read in the selected GM project file and show progress of read
+            using (ProjectIOForm form = new ProjectIOForm(file, true))
+            {
+                form.ShowDialog();
+                return form.GMProject;
+            }
+        }
+
+        /// <summary>
+        /// Gets objects
+        /// </summary>
+        /// <param name="file">The path to the Game Maker project file</param>
+        public static bool GetObjects(string file)
+        {
+            // Create a new Game Maker project form
+            using (ProjectIOForm form = new ProjectIOForm(file, true))
+            {
+                // Show the form
+                form.ShowDialog();
+
+                // If the project is empty, return
+                if (form.GMProject == null || form.GMProject.ProjectTree == null || form.GMProject.ProjectTree.Nodes == null)
+                    return false;
+
+                // The object node index
+                int objectIndex = -1;
+
+                // Get the object node
+                for (int i = 0; i < form.GMProject.ProjectTree.Nodes.Length; i++)
+                    if (form.GMProject.ProjectTree.Nodes[i].ResourceType == GMResourceType.Objects)
+                        objectIndex = i;
+
+                // If no object node was found, return
+                if (objectIndex == -1)
+                    return false;
+
+                // Check instances for differences
+                CheckInstances(form.GMProject);
+
+                // Clear previous objects if any
+                App.Room.Objects.Clear();
+
+                // Iterate through project objects
+                foreach (GMObject resource in form.GMProject.Objects)
+                {
+                    // GMare object variables
+                    int originX = 0;
+                    int originY = 0;
+                    Bitmap image = null;
+
+                    // Get the sprite object
+                    GMSprite sprite = form.GMProject.Sprites.Find(i => i.Id == resource.SpriteId);
+
+                    // If a sprite was found
+                    if (sprite != null)
+                    {
+                        // Get sprite data
+                        originX = sprite.OriginX;
+                        originY = sprite.OriginY;
+
+                        // If there are no sub-images, skip image data
+                        if (sprite.SubImages.Length <= 0)
+                        {
+                            // Add new gmare object
+                            App.Room.Objects.Add(new GMareObject(resource, image, resource.SpriteId, resource.Depth, originX, originY));
+                            continue;
+                        }
+
+                        image = GameMaker.Common.GMUtilities.GetBitmap(sprite.SubImages[0]);
+
+                        // Set transparency if needed
+                        if (sprite.Transparent && sprite.SubImages[0].Compressed)
+                            image.MakeTransparent(image.GetPixel(0, image.Height - 1));
+                    }
+
+                    // Add new gmare object
+                    App.Room.Objects.Add(new GMareObject(resource, image, resource.SpriteId, resource.Depth, originX, originY));
+                }
+
+                // Get the object nodes
+                App.Room.Nodes = form.GMProject.ProjectTree.Nodes[objectIndex].Nodes;
+
+                // Set textures for loaded objects
+                SetTextures();
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Set textures for loaded objects
+        /// </summary>
+        public static void SetTextures()
+        {
+            // Delete all previous textures if any
+            Graphics.GraphicsManager.DeleteTextures();
+
+            // If a room was not loaded, return
+            if (App.Room == null)
+                return;
+
+            // Iterate though new objects, add sprite texture
+            foreach (GMareObject obj in App.Room.Objects)
+                Graphics.GraphicsManager.LoadTexture(obj.Image, obj.Resource.Id);
+        }
+
+        /// <summary>
+        /// Checks existing instances against newly imported objects
+        /// </summary>
+        /// <param name="project">The Game Maker project to check against</param>
+        private static void CheckInstances(GMProject project)
+        {
+            // If no instances exist, return
+            if (App.Room.Instances.Count == 0)
+                return;
+
+            // Get object node
+            List<Image> images = new List<Image>();
+            TreeNode root = GMUtilities.GetTreeNodeFromGMNode(project, project.ProjectTree.Nodes[7], images);
+
+            // A list of checked object resources
+            Dictionary<int, GMResource> ids = new Dictionary<int, GMResource>();
+
+            // A list of object nodes
+            List<TreeNode> nodes = new List<TreeNode>();
+
+            // Get all the child nodes
+            foreach (TreeNode node in root.Nodes)
+                nodes.Add(node);
+
+            // Iterate through existing instances
+            foreach (GMareInstance instance in App.Room.Instances)
+            {
+                // If the instance's resource id does not match any new objects
+                if (project.Objects.Find(o => o.Id == instance.ObjectId ||  o.Name == instance.ObjectName) == null)
+                {
+                    // If the id check has already processed the instance's object id
+                    if (ids.ContainsKey(instance.ObjectId) == true)
+                    {
+                        // Set new instance data.
+                        int key = instance.ObjectId;
+                        instance.ObjectId = ids[key].Id;
+                        instance.ObjectName = ids[key].Name;
+                        continue;
+                    }
+
+                    // Get original glyph graphic
+                    Bitmap glyph = App.Room.Objects.Find(o => o.Resource.Id == instance.ObjectId).Image.ToBitmap();
+
+                    // Give the user a choice of either replacing the instance's parent object id, or just delete them
+                    using (InstanceConflictForm form = new InstanceConflictForm(nodes.ToArray(), instance.ObjectName, images.ToArray(), glyph))
+                    {
+                        // Show the instance options form
+                        form.ShowDialog();
+
+                        // Set resource as one that has been checked
+                        ids.Add(instance.ObjectId, form.Object);
+
+                        // Iterate through blocks
+                        foreach (GMareInstance block in App.Room.Blocks)
+                        {
+                            if (block.ObjectId == instance.ObjectId)
+                            {
+                                block.ObjectId = form.Object.Id;
+                                block.ObjectName = form.Object.Name;
+                            }
+                        }
+
+                        // Set instance with new data
+                        instance.ObjectId = form.Object.Id;
+                        instance.ObjectName = form.Object.Name;
+                    }
+                }
+            }
+
+            // Dispose
+            foreach (Image img in images)
+                img.Dispose();
+
+            // Remove all instances that have an empty id
+            App.Room.Instances.RemoveAll(i => i.ObjectId == -1);
+            App.Room.Blocks.RemoveAll(b => b.ObjectId == -1);
+        }
+
+        #endregion
+    }
+}
